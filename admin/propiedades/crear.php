aa6314e7cc9ba6388c7fdd122ea8b93c56a3976d
PABLO GRAMAJO <pgramajox@miumg.edu.gt>
2025-10-07 10:21:39 -0600

    Se agrego parte del diseño de la creación de propiedades y se conecto a la BD.

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,6 +1,30 @@
 <?php
 
 require '../../includes/funciones.php';
+require '../../includes/config/database.php';
+
+$db = conectarDB();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $titulo = $_POST['titulo'];
+    $precio = $_POST['precio'];
+    $descripcion = $_POST['descripcion'];
+    $habitaciones = $_POST['habitaciones'];
+    $wc = $_POST['wc'];
+    $estacionamiento = $_POST['estacionamiento'];
+    $vendedor_id = $_POST['vendedor_id'];
+
+    $query = "INSERT INTO propiedad (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedor_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedor_id')";
+
+    $resultado = mysqli_query($db, $query);
+
+    if ($resultado) {
+        echo "Insertado Correctamente";
+    }
+
+    //echo $query;
+}
 
 incluirTemplate('header');
 ?>
@@ -9,6 +33,49 @@ incluirTemplate('header');
     <h1>Crear</h1>
 
     <a href="/admin" class="boton boton-verde">Volver</a>
+
+    <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+        <fieldset>
+            <legend>Información General</legend>
+
+            <label for="titulo">Titulo:</label>
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" p>
+
+            <label for="precio">Precio:</label>
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" p>
+
+            <label for="imagen">Imagen:</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png" p>
+
+            <label for="descripcion">descripció:</label>
+            <textarea id="descripcion" name="descripcion"></textarea>
+
+        </fieldset>
+
+        <fieldset>
+            <legend>Información Propiedad</legend>
+
+            <label for="habitaciones">Habitaciones:</label>
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" p>
+
+            <label for="wc">Baños:</label>
+            <input type="number" id="wc" name="wc" placeholder="Ej: 1" min="1" max="9" p>
+
+            <label for="estacionamiento">Estacionamiento:</label>
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 2" min="1" max="9" p>
+        </fieldset>
+
+        <fieldset>
+            <legend>Vendedor</legend>
+
+            <select name="vendedor_id">
+                <option value="1">Carlos</option>
+                <option value="2">Ana</option>
+            </select>
+        </fieldset>
+
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+    </form>
 </main>
 
 <?php
